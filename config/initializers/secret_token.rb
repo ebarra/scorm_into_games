7e6093de783a08869f0ecd12e2e889a55a76fd1d
Enrique Barra <enrique.barra.arias@gmail.com>
2013-03-21 09:02:22 +0100

    first scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '20181c3bc2142ca9aee437e479e3a2c7feb01a0aac73a68e3513f2a690129260f12cbc7297d05abf37f3ff6dbc25f108c99389f20f4e19628a12e80cc3ea3f85'
