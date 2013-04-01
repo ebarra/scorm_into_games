5f382b202d732ee8a41190d8a47b2b05b51a2383
Aldo Gordillo <agordillos@gmail.com>
2013-04-01 19:17:05 +0200

    Second version of the API: fancybox and LO observer

--- public/jsapi/SGAME.js
+++ public/jsapi/SGAME.js
@@ -1,3 +1,4 @@
+var global;
 /**
  * SGAME JavaScript API
  * Integrate SCORM into web games
@@ -41,12 +42,168 @@ SGAME = (function(undefined){
 	//////////
 	var fancybox = (function(undefined){
 
+		var _currentFancybox = undefined;
+
 		var init = function(){
 
 		}
 
+		var create = function(options,onCloseCallback){
+			_removeCurrentFancybox();
+
+			//Params
+			var width = 850;
+			var height = 650;
+			var url;
+
+			if(options){
+				if(options.width){
+					width = options.width;
+				}
+				if(options.height){
+					height = options.height;
+				}
+				if(options.url){
+					url = options.url;
+				}
+			}
+
+			//Params Validation
+			if(!url){
+				return;
+			}
+
+			var fancybox = document.createElement('div');
+			fancybox.style.width = width + "px";
+			fancybox.style.height = height + "px";
+			fancybox.style.overflow = "hidden";
+			fancybox.style.background = 'white';
+			fancybox.style.position = "absolute";
+			fancybox.style.top = 0;
+			fancybox.style.zindex = 9999;
+			fancybox.style.borderRadius = '1em';
+
+			fancybox.setAttribute('id', "test");
+
+			var marginLeft = (window.innerWidth - width)/2;
+			fancybox.style.marginLeft = marginLeft + "px";
+
+			var marginTop = (window.innerHeight - height)/2;
+			fancybox.style.marginTop = marginTop + "px";
+
+			//Close button
+			var closeButton = document.createElement('img'); 
+			closeButton.src = "/assets/close.png";
+			closeButton.style.width = "25px";
+			closeButton.style.height = "25px";
+			closeButton.style.padding = "5px";
+			closeButton.style.cursor = "pointer";
+			closeButton.style.position = "absolute";
+			closeButton.style.right = 0;
+			closeButton.onclick = function(){
+				_removeCurrentFancybox();
+				if(typeof onCloseCallback === "function"){
+					var report = observer.getReport();
+					onCloseCallback(report);
+				}
+			}
+			fancybox.appendChild(closeButton);
+
+			//Iframe
+			var iframe = document.createElement('iframe');
+			iframe.src = url;
+			iframe.style.width = "95%";
+			iframe.style.height = "95%";
+			iframe.style.marginLeft = "2.5%";
+			iframe.style.marginTop = "30px";
+			iframe.style.overflow = "hidden";
+			iframe.scrolling = "no";
+			iframe.style.frameBorder = "0";
+			iframe.style.borderStyle="none";
+			fancybox.appendChild(iframe);
+
+			//Add observer
+			observer.start(iframe);
+
+			_currentFancybox = fancybox;
+			document.body.appendChild(fancybox);
+		}
+
+		var _removeCurrentFancybox = function(){
+			if(!_currentFancybox){
+				return;
+			}
+			//Hide fancybox
+			_currentFancybox.style.display = "none";
+			//Remove fancybox
+			_currentFancybox.parentNode.removeChild(_currentFancybox);
+			_currentFancybox = undefined;
+		}
+
 		return {
-			init 			: init
+			init 			: init,
+			create			: create
+		};
+
+	}) ();
+
+
+	///////////
+	// Observer
+	//////////
+	var observer = (function(undefined){
+
+		var _currentIframe = undefined;
+		var _eventsLoaded = false;
+
+		//Params
+		var startTime;
+		var success;
+
+		var start = function(iframe){
+			if(iframe){
+				_loadEvents();
+				_currentIframe = iframe;
+
+				//Reset params
+				startTime = Date.now();
+				success = false;
+			}
+		}
+
+		var _loadEvents = function(){
+			if(_eventsLoaded){
+				return;
+			}
+			if (window.addEventListener){
+				window.addEventListener("message", _onMessage, false);
+			} else if (el.attachEvent){
+				window.addEventListener("message", _onMessage);
+			}
+			_eventsLoaded = true;
+		}
+
+		var _onMessage = function(message){
+		}
+
+		var getReport = function(){
+			var timeSpent = Math.round((Date.now() - startTime)/1000);
+
+			//First version
+			//Success only depends of timeSpent
+			success = timeSpent > 3;
+
+			var report = {
+				time: timeSpent,
+				success: success
+			};
+
+			return report;
+		}
+
+		return {
+			start		: start,
+			getReport	: getReport
 		};
 
 	}) ();
@@ -55,10 +212,26 @@ SGAME = (function(undefined){
 	//Init debug module for developping
 	deb.init(true);
 
+
+
+	//////////////
+	// SGAME Module
+	//////////////
+
 	//vars
 	var _settings;
 	var _event_mapping = {};
 	var _togglePauseFunction = undefined;
+
+
+	/**
+	 * SGAME API
+	 * 
+	 * init
+	 * loadSeetings
+	 * triggerLO
+	 * showLO
+	 **/
 
 	var init = function(options) {
 		deb.log("SGAME init with options ");
@@ -70,12 +243,6 @@ SGAME = (function(undefined){
 			}
 		}
 	};
-
-	var _togglePause = function(){
-		if(typeof _togglePauseFunction === "function"){
-			_togglePauseFunction();
-		}
-	}
 
 	var loadSettings = function(settings){
 		deb.log("SGAME load settings ");
@@ -90,9 +257,8 @@ SGAME = (function(undefined){
 		}
 	}
 
-
 	var triggerLO = function(event_id,callback){
-		var los_candidate = _event_mapping[event_id];
+		var los_candidate = _event_mapping[event_id].los;
 		if(los_candidate){
 			var loId = _randomChoice(los_candidate);
 			_renderLO(loId,callback);
@@ -101,7 +267,7 @@ SGAME = (function(undefined){
 
 
 	/*
-	 * Uses cases:
+	 * Use cases:
 	 * showLO(callback)
 	 * showLO(options,callback)
 	 */
@@ -127,24 +293,37 @@ SGAME = (function(undefined){
 	};
 
 
+	//////////////
+	// Utils
+	//////////////
+
 	var _renderLO = function(loId,callback){
-		//TODO: render LO
 		_togglePause();
-		var result = confirm('¿Quieres consumir este LO?');
-		if(typeof callback == "function"){
-			callback(result);
-			_togglePause();
-		}
+
+		//TODO: get LO metadata
+
+		fancybox.create({ url: "/lo/" + loId}, function(report){
+			deb.log("LO report");
+			deb.log(report);
+			if(typeof callback == "function"){
+				callback(report.success);
+				_togglePause();
+			}
+		});
 	}
 
-
-	//Utils
 	var _randomChoice = function(box){
 		if(typeof box == "number"){
 			return box; //one single element
 		} else {
 			//TODO: random
 			return box[0];
+		}
+	}
+
+	var _togglePause = function(){
+		if(typeof _togglePauseFunction === "function"){
+			_togglePauseFunction();
 		}
 	}
 
@@ -156,6 +335,8 @@ SGAME = (function(undefined){
 	};
 
 }) ();
+
+
 
 //Metadata
 SGAME.VERSION = '0.1';
